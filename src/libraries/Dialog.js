e7130b590fc8db23e36f6c62c22a3def24d26c20
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-02-24 21:38:55 +0100

    feat: cleanup

--- src/libraries/Dialog.js
+++ src/libraries/Dialog.js
@@ -1,15 +1,15 @@
-const SELECTOR_DEFAULT = '.lib-dialog[data-type="dynamic"]'
-
-const SHOW_DEFAULT = {
-    content: undefined ?? null,
-    selector: SELECTOR_DEFAULT ?? null,
-    remove: false ?? null,
-    append: false ?? null
-}
-
-const CLOSE_DEFAULT = {
-    selector: SELECTOR_DEFAULT ?? null,
-    remove: false ?? null
+const DEFAULTS = {
+    openClass: 'is-lib-dialog-open',
+    show: {
+        content: undefined ?? null,
+        selector: '.lib-dialog[data-type="dynamic"]' ?? null,
+        remove: false ?? null,
+        append: false ?? null
+    },
+    close: {
+        selector: '.lib-dialog[data-type="dynamic"]' ?? null,
+        remove: false ?? null
+    }
 }
 
 const dialogSelector = selector => typeof selector === 'string'
@@ -22,12 +22,12 @@ const dialogDismiss = async (options) => {
     dialogSelector(options.selector).setAttribute('inert', '')
 
     if (!document.querySelector('dialog[open]')) {
-        document.documentElement.classList.remove('is-lib-dialog-open')
+        document.documentElement.classList.remove(DEFAULTS.openClass)
     }
 }
 
-const showDialog = async (options = SHOW_DEFAULT) => {
-    options = Object.assign({}, SHOW_DEFAULT, options)
+const showDialog = async (options = DEFAULTS.show) => {
+    options = Object.assign({}, DEFAULTS.show, options)
 
     if (options.content && !options.append && dialogSelector(options.selector)) {
         dialogSelector(options.selector)?.firstElementChild?.remove()
@@ -62,12 +62,12 @@ const showDialog = async (options = SHOW_DEFAULT) => {
         ? dialogSelector(options.selector).showModal()
         : dialogSelector(options.selector).setAttribute('open', '')
 
-    document.documentElement.classList.add('is-lib-dialog-open')
+    document.documentElement.classList.add(DEFAULTS.openClass)
     document.documentElement.style.setProperty('--lib-dialog-scrollbar-width', `${window.innerWidth - document.body.clientWidth}px`)
 }
 
-const closeDialog = async (options = CLOSE_DEFAULT) => {
-    options = Object.assign({}, CLOSE_DEFAULT, options)
+const closeDialog = async (options = DEFAULTS.close) => {
+    options = Object.assign({}, DEFAULTS.close, options)
 
     window.HTMLDialogElement
         ? dialogSelector(options.selector).close()
