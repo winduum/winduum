0a5c2a0907f13e7f7e873a1be23caf430dbf164f
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-03-12 17:58:31 +0100

    feat: examples update

--- examples/vite/src/main.js
+++ examples/vite/src/main.js
@@ -1,2 +1,14 @@
 // to adding support for :has selector for unsupported browsers
 !CSS.supports('selector(:has(*))') && (async() => (await import('css-has-pseudo/browser')).default(document))()
+
+export function setupCounter(element) {
+    let counter = 0
+    const setCounter = (count) => {
+        counter = count
+        element.innerHTML = `count is ${counter}`
+    }
+    element.addEventListener('click', () => setCounter(counter + 1))
+    setCounter(0)
+}
+
+setupCounter(document.querySelector('#counter'))
