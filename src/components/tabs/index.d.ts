3f02885eba107188ecba552c568a873d775a9073
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-02-15 14:03:59 +0100

    feat: tabs finish

---
+++ src/components/tabs/index.d.ts
@@ -0,0 +1,4 @@
+interface ToggleTabOptions {
+    tabElements?: NodeListOf<Element>
+    tabPanelElements?: NodeListOf<Element>
+}
--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -1,24 +1,13 @@
-export const toggleTabs = (element, options = {}) => {
-    options = {
-        activeClass: '',
-        inactiveClass: '',
-        tabControlElements: null,
-        tabPanelElements: null
-    }
-
-    if (!options.tabPanelElements) return
-
-    options.tabPanelElements.forEach(target => (target.ariaHidden = 'true'))
+/**
+ * @param {HTMLElement | Element} element - The dialog element to show.
+ * @param {import("./").ToggleTabOptions} options - The options for showing the dialog.
+ * @returns void
+ */
+export const toggleTab = (element, options = {}) => {
+    options.tabElements?.forEach(element => (element.ariaSelected = 'false'))
+    options.tabPanelElements?.forEach(target => (target.ariaHidden = 'true'))
+    element.ariaSelected = 'true'
 
     const ariaControls = document.getElementById(element.getAttribute('aria-controls'))
-
     if (ariaControls) (ariaControls.ariaHidden = 'false')
-}
-
-export const removeActiveClasses = () => {
-
-}
-
-export const addActiveClasses = () => {
-
 }
