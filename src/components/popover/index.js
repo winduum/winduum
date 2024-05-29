e7d10f6242361b0bab36fe9a9dcaec4e028610b9
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-05-29 16:36:52 +0200

    feat: added popover api

---
+++ src/components/popover/index.js
@@ -0,0 +1,60 @@
+import { animationsFinished } from '../../common.js'
+
+export const computePopover = async (element, popover, options) => {
+    const { computePosition, flip, shift, offset } = await import('@floating-ui/dom')
+
+    popover.classList.remove(popover._placement)
+
+    popover.style.minWidth = `${element.offsetWidth / 16}rem`
+
+    await computePosition(element, popover, {
+        placement: options?.placement,
+        middleware: options?.middleware ?? [offset(12 ?? options?.offset), flip(), shift({ padding: 8, ...options?.shift })]
+    }).then(({ x, y, placement }) => {
+        Object.assign(popover.style, {
+            inset: `${y}px auto auto ${x}px`
+        })
+
+        popover._placement = placement
+        popover.classList.add(popover._placement, options?.visibleClass ?? 'in')
+    })
+}
+
+export const closePopover = async (element) => {
+    const popoverElement = window[element.getAttribute('popovertarget')]
+
+    popoverElement.classList.remove('in')
+    await animationsFinished(popoverElement)
+    popoverElement._cleanup()
+    popoverElement.hidePopover && popoverElement.hidePopover()
+
+    element.ariaExpanded = false
+}
+
+export const showPopover = async (element, options) => {
+    const { autoUpdate } = await import('@floating-ui/dom')
+
+    const popoverElement = window[element.getAttribute('popovertarget')]
+
+    element.ariaExpanded = true
+
+    if (!element.ariaHasPopup) (element.ariaHasPopup = 'dialog')
+    if (!popoverElement.role) (popoverElement.role = element.ariaHasPopup)
+    if (!popoverElement.popover) (popoverElement.popover = 'manual')
+
+    popoverElement.showPopover && popoverElement.showPopover()
+
+    popoverElement._cleanup = autoUpdate(
+        element,
+        popoverElement,
+        async () => await computePopover(element, popoverElement, options?.computePosition)
+    )
+}
+
+export const togglePopover = async (element, options) => {
+    if (element.ariaExpanded !== 'true') {
+        await showPopover(element, options)
+    } else {
+        await closePopover(element)
+    }
+}
