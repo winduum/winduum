58c50d97507acf81003f653e675918dab8e961f7
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2024-09-04 19:38:59 +0200

    feat: build

--- types/index.d.ts
+++ types/index.d.ts
@@ -129,7 +129,7 @@ declare module 'winduum/src/components/drawer' {
 
 	export function showDrawer(element: HTMLElement | Element, distance?: number, direction?: 'left' | 'top'): void
 	export function closeDrawer(element: HTMLElement | Element, distance?: number, direction?: 'left' | 'top'): void
-	export function scrollInitDrawer(element: HTMLElement | Element, distance?: number, direction?: 'left' | 'top', timeout?: number): void
+	export function scrollInitDrawer(element: HTMLElement | Element, distance?: number, direction?: 'left' | 'top'): void
 	export function scrollDrawer(element: HTMLDialogElement | Element, options?: ScrollDrawerOptions): void
 
 	export {};
