e57b66df550308c8da2328076c115a8b137838b3
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2025-08-01 23:17:54 +0200

    feat: stylelint.config.js

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    '@stylistic/stylelint-config',
+    'stylelint-config-standard',
+  ],
+  rules: {
+    '@stylistic/max-line-length': [240, { ignorePattern: '/^\\s*--/' }],
+    'at-rule-no-unknown': [true, { ignoreAtRules: ['layer', 'tailwind', 'theme', 'utility', 'variant', 'custom-variant', 'source'] }],
+    'alpha-value-notation': ['percentage'],
+    'declaration-property-value-no-unknown': [true, { ignoreProperties: { 'appearance': ['base-select'], '-webkit-line-break': ['after-white-space'] } }],
+    'import-notation': 'string',
+  },
+  ignoreFiles: ['dist/**', 'src/theme/config/font.css', 'src/theme/config/spacing.css'],
+}
