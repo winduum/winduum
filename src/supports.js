965332ee6ffa0cebbf3a7fea631338bc7a8a7d22
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-06-22 08:26:12 +0200

    feat: implemented x-drawer custom element with enhanced scroll and animation support

--- src/supports.js
+++ src/supports.js
@@ -7,3 +7,13 @@ export const supportsTimelineTrigger = CSS.supports('timeline-trigger-name: --v'
  * @type {boolean}
  */
 export const supportsInterestFor = Object.prototype.hasOwnProperty.call(HTMLButtonElement.prototype, 'interestForElement')
+
+/**
+ * @type {boolean}
+ */
+export const supportsScrollInitialTarget = CSS.supports('scroll-initial-target', 'nearest')
+
+/**
+ * @type {boolean}
+ */
+export const supportsAnimationTimeline = CSS.supports('animation-timeline: scroll()')
